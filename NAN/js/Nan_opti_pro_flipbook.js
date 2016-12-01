936a4da7ba25319740d9faa8fef01b8128f47cff
German_Topalov <german.topalov@breffi.ru>
2016-12-01 17:39:09 +0600

    Убрал лого, убрал центровку флипбука во избежание его "скачков"

--- NAN/js/Nan_opti_pro_flipbook.js
+++ NAN/js/Nan_opti_pro_flipbook.js
@@ -1,43 +1,16 @@
 ﻿$(function () {
 	flipbook = $("#flipbook");
-	prevPage = $("#flipbookPrevPage");
-	nextPage = $("#flipbookNextPage");
-
 
 	flipbook.turn({
 		width: 700,
 		height: 450,
-		autoCenter: true,
+		//autoCenter: true,
 		acceleration: true,
 		duration: 1200,
 	});
-	totalPages = flipbook.turn("pages");
-	if(flipbook.turn("page") == 1) {
-		prevPage.hide();
-	}
+
 
 	flipbook.bind("turning",function(event, page, view){
 		storyCLMNavigation.blockSwipe();
-	});
-
-	prevPage.click(function(){
-		flipbook.turn("previous");
-		if(flipbook.turn("page") == 1) {
-			prevPage.hide();
-		}
-		else {
-			nextPage.show();
-		};
-	});
-
-	nextPage.click(function(){
-		flipbook.turn("next");
-
-		if(flipbook.turn("page") == totalPages) {
-			nextPage.hide();
-		}
-		else {
-			prevPage.show();
-		};
 	});
 });
