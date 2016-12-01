0eda9b411bc0520e41ba14c09543ce8056960b36
German_Topalov <german.topalov@breffi.ru>
2016-12-01 17:14:20 +0600

    Добавил метод блокировки storyclmnav, убрал стрелки навигации

--- NAN/js/Nan_opti_pro_flipbook.js
+++ NAN/js/Nan_opti_pro_flipbook.js
@@ -12,10 +12,13 @@
 		duration: 1200,
 	});
 	totalPages = flipbook.turn("pages");
-
 	if(flipbook.turn("page") == 1) {
 		prevPage.hide();
 	}
+
+	flipbook.bind("turning",function(event, page, view){
+		storyCLMNavigation.blockSwipe();
+	});
 
 	prevPage.click(function(){
 		flipbook.turn("previous");
